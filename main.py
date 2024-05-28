7c31b44843a5d3b5fef45aa6794ad5e57c830254
Alex Lopez <75276270+alexflopez@users.noreply.github.com>
2024-05-28 14:39:22 -0300

    Update main.py
    Forcing all df to String, timestamp included for personal control.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 import pandas_gbq
 from google.oauth2 import service_account
 from google.cloud import bigquery
-
+import datetime
 
 # Reading a csv using pandas
-df = pd.read_csv('response.csv', delimiter=",")
+df = pd.read_csv('response.csv', delimiter=",").astype(str)
+df.drop(df.columns[0], axis=1, inplace=True)
 #print(df)
 
 # Sending the raw data to Google BigQuery table
@@ -22,6 +23,6 @@ try:
                     project_id=project_id,
                     credentials=credentials,
                     if_exists="replace")# replacing table to a brand new one table
-  print('Data has been sent to BigQuery')
+  print(f'Data has been sent to BigQuery \n{datetime.datetime.now()}')
 except Exception as e:
   print(f'Error: {e}')
